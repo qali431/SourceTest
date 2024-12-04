e68d535e80a52adda314e79571943127cfefceab
qali431 <qali@telus.net>
2024-12-03 17:40:49 -0700

    Initial Commit

---
+++ SourceTest/SourceTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceTestApp.swift
+//  SourceTest
+//
+//  Created by Qamber Ali on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct SourceTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
